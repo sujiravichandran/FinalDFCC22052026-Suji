218840e5e8b05785a5543818a56186c29f98a7eb
vignesh <vignesh.k@teclever.com>
2024-05-18 09:36:18 +0530

    initial commit

---
+++ src/com/teclever/dfcc/model/VDDConfiguraion.java
@@ -0,0 +1,30 @@
+package com.teclever.dfcc.model;
+
+public class VDDConfiguraion {
+
+	private String fileName;
+	private String checksumValue;
+	private String path;
+	
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getChecksumValue() {
+		return checksumValue;
+	}
+	public void setChecksumValue(String checksumValue) {
+		this.checksumValue = checksumValue;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+	
+}
