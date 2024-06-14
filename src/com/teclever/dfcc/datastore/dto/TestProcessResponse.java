c359b0070b5866963a09f93fc9439d26b12b6be8
vijayakumar M N <vijayakumar.mn@teclever.com>
2024-06-14 11:58:50 +0530

    TestProcessManagement update
    Modified TestProcessControl method

--- src/com/teclever/dfcc/datastore/dto/TestProcessResponse.java
+++ src/com/teclever/dfcc/datastore/dto/TestProcessResponse.java
@@ -1,19 +1,34 @@
 
 package com.teclever.dfcc.datastore.dto;
+
 import java.util.Map;
 import com.teclever.datastore.dto.Response;
+
 public class TestProcessResponse {
 	private Map<String, String> testProcessResult;
+	private int dStarCount;
 	private Response response;
+
 	public Map<String, String> getTestProcessResult() {
 		return testProcessResult;
 	}
+
 	public void setTestProcessResult(Map<String, String> testProcessResult) {
 		this.testProcessResult = testProcessResult;
 	}
+
+	public int getdStarCount() {
+		return dStarCount;
+	}
+
+	public void setdStarCount(int dStarCount) {
+		this.dStarCount = dStarCount;
+	}
+
 	public Response getResponse() {
 		return response;
 	}
+
 	public void setResponse(Response response) {
 		this.response = response;
 	}
