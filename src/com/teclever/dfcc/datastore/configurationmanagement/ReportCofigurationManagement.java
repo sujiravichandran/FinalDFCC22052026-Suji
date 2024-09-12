aaf443386c5d63c06c910ce219086d4dd2e0f9b1
vijayakumar M N <vijayakumar.mn@teclever.com>
2024-09-12 10:51:10 +0530

    Report Configuration Management
    -AddReportConfig()
    -GetAllReportConfig()
         -Class  :
          ReportConfigDto.
          ReportConfigResponse.
          ReportConfigurationManagement.

---
+++ src/com/teclever/dfcc/datastore/configurationmanagement/ReportCofigurationManagement.java
@@ -0,0 +1,109 @@
+package com.teclever.dfcc.datastore.configurationmanagement;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import com.teclever.datastore.dto.GetObjResponse;
+import com.teclever.datastore.dto.Response;
+import com.teclever.datastore.entities.ReportConfig;
+import com.teclever.datastore.service.LevelFiveMasterService;
+import com.teclever.datastore.service.LevelFourMasterSevice;
+import com.teclever.datastore.service.LevelOneMasterService;
+import com.teclever.datastore.service.LevelThreeService;
+import com.teclever.datastore.service.LevelTwoMasterService;
+import com.teclever.datastore.service.ReportService;
+import com.teclever.datastore.utils.GetResponse;
+import com.teclever.dfcc.datastore.dto.ReportConfigDto;
+import com.teclever.dfcc.datastore.dto.ReportConfigResponse;
+import com.teclever.dfcc.stateMachine.StateMachine.currentSessionDetails;
+
+public class ReportCofigurationManagement {
+
+	public Response addReportConfig(ReportConfigDto reportConfigDto) {
+		Response res = new Response();
+		try {
+			ReportService reportService = new ReportService();
+			GetObjResponse getObjResponse = reportService.addReportConfig(reportConfigDto.getFileNameWitFullPath(),
+					currentSessionDetails.getSessionId(), reportConfigDto.getReportType(),
+					reportConfigDto.getLevelOneId(), reportConfigDto.getLevelTwoId(), reportConfigDto.getLevelThreeId(),
+					reportConfigDto.getLevelFourId(), reportConfigDto.getLevelFiveId());
+			res = getObjResponse.getResponse();
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setResponseCode(0);
+			res.setResponseMessage("Data Save Unsucessfull ");
+		}
+		return res;
+	}
+
+	public ReportConfigResponse getAllReportConfig(String reportType) {
+		ReportConfigResponse reportConfigResponse = new ReportConfigResponse();
+		Response res = new Response();
+		try {
+			ReportService reportService = new ReportService();
+			GetResponse getResponse = reportService.getReportCofigDetails(currentSessionDetails.getSessionId(),
+					reportType);
+			List<?> listOfReportConfig = getResponse.getResponseList();
+			List<ReportConfigDto> listOfReportConfigDto = new ArrayList<>();
+
+			LevelOneMasterService levelOneService = new LevelOneMasterService();
+			Map<String, String> levelOneStage = levelOneService.getAllLevelOneIdAndLevelName();
+
+			LevelTwoMasterService levelTwoService = new LevelTwoMasterService();
+			Map<String, String> levelTwoStage = levelTwoService.getAllLevelIdAndLevelName();
+
+			LevelThreeService levelThreeService = new LevelThreeService();
+			Map<String, String> levelThreeStage = levelThreeService.getAllLevelIdAndLevelName();
+
+			LevelFourMasterSevice levelFourService = new LevelFourMasterSevice();
+			Map<String, String> levelFourStage = levelFourService.getAllLevelIdAndLevelName();
+
+			LevelFiveMasterService levelFiveService = new LevelFiveMasterService();
+			Map<String, String> levelFiveStage = levelFiveService.getAllLevelIdAndLevelName();
+
+			for (Object reportConfigObject : listOfReportConfig) {
+
+				ReportConfig repoConfig = (ReportConfig) reportConfigObject;
+				ReportConfigDto reportConfigDto = new ReportConfigDto();
+
+				reportConfigDto.setReportConfigId(repoConfig.getReportConfigId());
+				reportConfigDto.setFileNameWitFullPath(Arrays.asList(repoConfig.getFileNameWitFullPath().split(",")));
+				reportConfigDto.setReportType(repoConfig.getReportType());
+
+				reportConfigDto.setLevelOneId(repoConfig.getLevelOneId());
+				reportConfigDto.setLevelOneName(levelOneStage.get(repoConfig.getLevelOneId()));
+
+				reportConfigDto.setLevelTwoId(repoConfig.getLevelTwoId());
+				reportConfigDto.setLevelTwoName(levelTwoStage.get(repoConfig.getLevelTwoId()));
+
+				reportConfigDto.setLevelThreeId(repoConfig.getLevelThreeId());
+				reportConfigDto.setLevelThreeName(levelThreeStage.get(repoConfig.getLevelThreeId()));
+
+				reportConfigDto.setLevelFourId(repoConfig.getLevelFourId());
+				reportConfigDto.setLevelFourName(levelFourStage.get(repoConfig.getLevelFourId()));
+
+				reportConfigDto.setLevelFiveId(repoConfig.getLevelFiveId());
+				reportConfigDto.setLevelFiveName(levelFiveStage.get(repoConfig.getLevelFiveId()));
+
+				listOfReportConfigDto.add(reportConfigDto);
+
+			}
+			reportConfigResponse.setListOfReportConfigDto(listOfReportConfigDto);
+			if(reportConfigResponse.getListOfReportConfigDto().size()==0) {
+				res.setResponseCode(0);
+				res.setResponseMessage("Data is Empty ");
+			}else {
+				res.setResponseCode(1);
+				res.setResponseMessage("Data Fetch Sucessfull ");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setResponseCode(0);
+			res.setResponseMessage("Data Fetch Unsucessfull ");
+		}
+		reportConfigResponse.setResponse(res);
+		return reportConfigResponse;
+	}
+}
---
+++ src/com/teclever/dfcc/datastore/dto/ReportConfigDto.java
@@ -0,0 +1,137 @@
+package com.teclever.dfcc.datastore.dto;
+
+import java.util.List;
+
+public class ReportConfigDto {
+
+	private String reportConfigId;// Primary Key
+
+	private List<String> fileNameWitFullPath;
+
+	private String reportType;// "PQT" "ESS" "DataPack"
+
+	private String levelOneName;
+
+	private String levelOneId;
+
+	private String levelTwoName;
+
+	private String levelTwoId;
+
+	private String levelThreeName;
+
+	private String levelThreeId;
+
+	private String levelFourName;
+
+	private String levelFourId;
+
+	private String levelFiveName;
+
+	private String levelFiveId;
+
+	public String getReportConfigId() {
+		return reportConfigId;
+	}
+
+	public void setReportConfigId(String reportConfigId) {
+		this.reportConfigId = reportConfigId;
+	}
+
+	public List<String> getFileNameWitFullPath() {
+		return fileNameWitFullPath;
+	}
+
+	public void setFileNameWitFullPath(List<String> fileNameWitFullPath) {
+		this.fileNameWitFullPath = fileNameWitFullPath;
+	}
+
+	public String getReportType() {
+		return reportType;
+	}
+
+	public void setReportType(String reportType) {
+		this.reportType = reportType;
+	}
+
+	public String getLevelOneId() {
+		return levelOneId;
+	}
+
+	public void setLevelOneId(String levelOneId) {
+		this.levelOneId = levelOneId;
+	}
+
+	public String getLevelTwoId() {
+		return levelTwoId;
+	}
+
+	public void setLevelTwoId(String levelTwoId) {
+		this.levelTwoId = levelTwoId;
+	}
+
+	public String getLevelThreeId() {
+		return levelThreeId;
+	}
+
+	public void setLevelThreeId(String levelThreeId) {
+		this.levelThreeId = levelThreeId;
+	}
+
+	public String getLevelFourId() {
+		return levelFourId;
+	}
+
+	public void setLevelFourId(String levelFourId) {
+		this.levelFourId = levelFourId;
+	}
+
+	public String getLevelFiveId() {
+		return levelFiveId;
+	}
+
+	public void setLevelFiveId(String levelFiveId) {
+		this.levelFiveId = levelFiveId;
+	}
+
+	public String getLevelOneName() {
+		return levelOneName;
+	}
+
+	public void setLevelOneName(String levelOneName) {
+		this.levelOneName = levelOneName;
+	}
+
+	public String getLevelTwoName() {
+		return levelTwoName;
+	}
+
+	public void setLevelTwoName(String levelTwoName) {
+		this.levelTwoName = levelTwoName;
+	}
+
+	public String getLevelThreeName() {
+		return levelThreeName;
+	}
+
+	public void setLevelThreeName(String levelThreeName) {
+		this.levelThreeName = levelThreeName;
+	}
+
+	public String getLevelFourName() {
+		return levelFourName;
+	}
+
+	public void setLevelFourName(String levelFourName) {
+		this.levelFourName = levelFourName;
+	}
+
+	public String getLevelFiveName() {
+		return levelFiveName;
+	}
+
+	public void setLevelFiveName(String levelFiveName) {
+		this.levelFiveName = levelFiveName;
+	}
+
+}
